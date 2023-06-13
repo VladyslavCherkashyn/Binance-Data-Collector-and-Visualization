73cec78ee87d75d44374a66f4f0241dd299b8efe
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-06-13 12:59:53 +0300

    now dataÑ is updated correctly

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-import plotly.graph_objects as go
+from flask import Flask, render_template, jsonify
 from binance.client import Client
 import configparser
 from binance.websockets import BinanceSocketManager
@@ -7,20 +6,16 @@ import time
 
 app = Flask(__name__, template_folder='templates')
 
-# Loading keys from config file
 config = configparser.ConfigParser()
 config.read_file(open('secret.cfg'))
 test_api_key = config.get('BINANCE', 'TEST_API_KEY')
 test_secret_key = config.get('BINANCE', 'TEST_SECRET_KEY')
 
-# Setting up connection
 client = Client(test_api_key, test_secret_key)
-client.API_URL = 'https://testnet.binance.vision/api'  # To change endpoint URL for test account
+client.API_URL = 'https://testnet.binance.vision/api'
 
-# Getting account info to check balance
-info = client.get_account()  # Getting account info
+info = client.get_account()
 
-# Saving different tokens and respective quantities into lists
 assets = []
 values = []
 for index in range(len(info['balances'])):
@@ -30,10 +25,9 @@ for index in range(len(info['balances'])):
         if key == 'free':
             values.append(info['balances'][index][key])
 
-token_usdt = {}  # Dict to hold pair price in USDT
-token_pairs = []  # List to hold different token pairs
+token_usdt = {}
+token_pairs = []
 
-# Creating token pairs and saving into a list
 for token in assets:
     if token != 'USDT':
         token_pairs.append(token + 'USDT')
@@ -102,77 +96,29 @@ def assets_usdt(assets, values, token_usdt):
     return assets_in_usdt
 
 
-# Streaming data for tokens in the portfolio
 bm = BinanceSocketManager(client)
 for tokenpair in token_pairs:
     conn_key = bm.start_symbol_ticker_socket(tokenpair, streaming_data_process)
 bm.start()
-time.sleep(10)  # To give sufficient time for all token pairs to establish connection
+time.sleep(5)
 
 
 @app.route('/')
 def index():
-    figure1 = go.Figure(
-        data=[go.Indicator(
-            mode="number",
-            value=total_amount_usdt(assets, values, token_usdt)
-        )],
-        layout=go.Layout(
-            title="Portfolio Value (USDT)"
-        )
-    )
+    return render_template('index.html')
 
-    figure2 = go.Figure(
-        data=[go.Indicator(
-            mode="number",
-            value=total_amount_btc(assets, values, token_usdt),
-            number={'valueformat': 'g'}
-        )],
-        layout=go.Layout(
-            title="Portfolio Value (BTC)"
-        )
-    )
 
-    figure3 = go.Figure(
-        data=[go.Indicator(
-            mode="number",
-            value=float(token_usdt['BNBUSDT']),
-            number={'valueformat': 'g'}
-        )],
-        layout=go.Layout(
-            title="BNB/USDT"
-        )
-    )
-
-    figure4 = go.Figure(
-        data=[go.Pie(
-            labels=assets,
-            values=assets_usdt(assets, values, token_usdt),
-            hoverinfo="label+percent"
-        )],
-        layout=go.Layout(
-            title="Portfolio Distribution (in USDT)"
-        )
-    )
-
-    figure5 = go.Figure(
-        data=[go.Bar(
-            x=assets,
-            y=values,
-            name="Token Quantities For Different Assets",
-        )],
-        layout=go.Layout(
-            showlegend=False,
-            title="Tokens Distribution"
-        )
-    )
-
-    return render_template('index.html',
-                           figure1=figure1.to_html(full_html=False, include_plotlyjs='cdn'),
-                           figure2=figure2.to_html(full_html=False, include_plotlyjs='cdn'),
-                           figure3=figure3.to_html(full_html=False, include_plotlyjs='cdn'),
-                           figure4=figure4.to_html(full_html=False, include_plotlyjs='cdn'),
-                           figure5=figure5.to_html(full_html=False, include_plotlyjs='cdn'))
+@app.route('/data')
+def get_data():
+    data = {
+        'total_amount_usdt': total_amount_usdt(assets, values, token_usdt),
+        'total_amount_btc': total_amount_btc(assets, values, token_usdt),
+        'token_usdt_bnb': float(token_usdt['BNBUSDT']),
+        'assets': assets,
+        'assets_usdt': assets_usdt(assets, values, token_usdt),
+        'values': values
+    }
+    return jsonify(data)
 
 
 if __name__ == '__main__':
